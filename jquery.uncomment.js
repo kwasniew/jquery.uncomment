583175e4733531fd97fb600ac615b9765a062020
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2014-09-17 16:17:23 +0200

    use constant instead of a magic number

--- jquery.uncomment.js
+++ jquery.uncomment.js
@@ -2,7 +2,7 @@
     $.fn.uncomment = function () {
         this.contents().
             filter(function() {
-                return this.nodeType === 8; // comment
+                return this.nodeType === Node.COMMENT_NODE;
             }).
             replaceWith(function() {
                 return this.data;
